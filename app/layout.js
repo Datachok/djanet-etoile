93e47d1c00d5baf48e6aa501bf511f7e566cb7cc
Linda Benboudiaf <38206428+linda-benboudiaf@users.noreply.github.com>
2026-05-02 18:02:46 +0100

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import StarrySky from "@/components/StarrySky";
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
           <main className="min-h-screen relative">{children}</main>
           <Footer />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
